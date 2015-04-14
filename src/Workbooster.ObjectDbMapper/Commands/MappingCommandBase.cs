0a5e18bcc832c2f5b636aa67ad5c463e8152c629
Roger Guillet <roger.guillet@workbooster.ch>
2015-04-14 10:30:18 +0200

    Created UpdateCommand. Added the shared command-logic for INSERT/UPDATE to an abstract base class.

---
+++ src/Workbooster.ObjectDbMapper/Commands/MappingCommandBase.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Workbooster.ObjectDbMapper.Reflection;
+
+namespace Workbooster.ObjectDbMapper.Commands
+{
+    public abstract class MappingCommandBase<T>
+    {
+        #region MEMBERS
+
+        /// <summary>
+        /// Key = ColumnName
+        /// Value = Mapping for the insert value
+        /// </summary>
+        protected Dictionary<string, Func<T, object>> _ColumnMappings = new Dictionary<string, Func<T, object>>();
+
+        #endregion
+
+        #region PROPERTIES
+
+        public DbConnection Connection { get; private set; }
+        public EntityDefinition Entity { get; private set; }
+
+        #endregion
+
+        #region PUBLIC METHODS
+
+        public MappingCommandBase(DbConnection connection)
+        {
+            Connection = connection;
+            Entity = ReflectionHelper.GetEntityDefinitionFromType<T>();
+
+            if (String.IsNullOrEmpty(Entity.DbTableName))
+            {
+                throw new Exception("Couldn't resolve the tablename. Please instantiate the command either with a tablename or add a [Table] attribute to the data class.");
+            }
+        }
+
+        public MappingCommandBase(DbConnection connection, string tableName)
+        {
+            Connection = connection;
+            Entity = ReflectionHelper.GetEntityDefinitionFromType<T>();
+            Entity.DbTableName = tableName;
+        }
+
+        /// <summary>
+        /// Creates or overwrites a mapping between a database column and a field from the data object.
+        /// Example: <code>cmd.Map("TypeName", o => { return o.IsCompany ? "Company" : "Person"; });</code>
+        /// </summary>
+        /// <param name="columnName"></param>
+        /// <param name="mappingFunction"></param>
+        /// <returns></returns>
+        public void Map(string columnName, Func<T, object> mappingFunction)
+        {
+            _ColumnMappings[columnName] = mappingFunction;
+        }
+
+        /// <summary>
+        /// Removes the field mapping for the given column.
+        /// </summary>
+        /// <param name="columnName"></param>
+        /// <returns></returns>
+        public bool RemoveMapping(string columnName)
+        {
+            return _ColumnMappings.Remove(columnName);
+        }
+
+        /// <summary>
+        /// Creates the column-to-field or column-to-property mappings dynamically by using reflection and the attributes.
+        /// Give a list with names of fields or properties which should be ignored or use RemoveMapping(string columnName) to remove unneeded columns.
+        /// </summary>
+        /// <param name="ignoredFieldsOrProperties">A list with names of fields or properties which should be ignored.</param>
+        public void CreateDynamicMappings(IEnumerable<string> ignoredFieldsOrProperties = null)
+        {
+            foreach (var fieldDefinition in Entity.FieldDefinitions)
+            {
+                if (ignoredFieldsOrProperties == null
+                    || ignoredFieldsOrProperties.Contains(fieldDefinition.MemberName) == false)
+                {
+                    Func<T, object> func = null;
+
+                    if (fieldDefinition.IsProperty)
+                    {
+                        func = delegate(T o)
+                        {
+                            return ((PropertyInfo)fieldDefinition.MemberInfo).GetValue(o, null);
+                        };
+                    }
+                    else
+                    {
+                        func = delegate(T o)
+                        {
+                            return ((FieldInfo)fieldDefinition.MemberInfo).GetValue(o);
+                        };
+                    }
+
+                    _ColumnMappings[fieldDefinition.DbColumnName] = func;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- src/Workbooster.ObjectDbMapper/Commands/UpdateCommand.cs
+++ src/Workbooster.ObjectDbMapper/Commands/UpdateCommand.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using Workbooster.ObjectDbMapper.Attributes;
 using Workbooster.ObjectDbMapper.Reflection;
 
 namespace Workbooster.ObjectDbMapper.Commands
 {
-    public class InsertCommand<T> : MappingCommandBase<T>
+    public class UpdateCommand<T> : MappingCommandBase<T>
     {
         #region PUBLIC METHODS
 
-        public InsertCommand(DbConnection connection) : base(connection) { }
+        public UpdateCommand(DbConnection connection) : base(connection) { }
 
-        public InsertCommand(DbConnection connection, string tableName) : base(connection, tableName) { }
+        public UpdateCommand(DbConnection connection, string tableName) : base(connection, tableName) { }
 
         public int Execute(IEnumerable<T> listOfItems)
         {
@@ -29,15 +27,14 @@ namespace Workbooster.ObjectDbMapper.Commands
 
             int numberOfRowsAffected = 0;
 
-            // prepare the SQL INSERT statement
-            string columnNames = String.Join(",", _ColumnMappings.Keys.ToArray());
-            string parameterNames = _ColumnMappings.Keys.Aggregate("", (acc, s) => acc += ",@" + s).Remove(0, 1);
-            string insertStatement = String.Format("INSERT INTO [{0}] ({1}) VALUES({2})", Entity.DbTableName, columnNames, parameterNames);
+            // prepare the SQL UPDATE statement
+            string parameterNames = _ColumnMappings.Keys.Aggregate("", (acc, s) => acc += "," + s +  "=@" + s).Remove(0, 1);
+            string updateStatement = String.Format("UPDATE [{0}] SET {1}", Entity.DbTableName, parameterNames);
 
             foreach (var item in listOfItems)
             {
                 DbCommand cmd = Connection.CreateCommand();
-                cmd.CommandText = insertStatement;
+                cmd.CommandText = updateStatement;
 
                 foreach (var mapping in _ColumnMappings)
                 {
